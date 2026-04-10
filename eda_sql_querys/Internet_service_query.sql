d117baaf7d0affa650cab9683181be7e4615fe01
Muhammad Ahmed Bin Riaz <ar2284296@gmail.com>
2026-04-10 22:50:40 +0500

    Update Internet_service_query.sql

--- eda_sql_querys/Internet_service_query.sql
+++ eda_sql_querys/Internet_service_query.sql
@@ -4,6 +4,3 @@ select InternetService , count(*) as total_customers , sum(binary_churn) as sum_
 	   , avg(binary_churn * 100) as percentage_chruned , round(avg(MonthlyCharges), 2) as monthly_charges
 from customer_staging
 group by InternetService;
-
-select *
-from customer_staging
